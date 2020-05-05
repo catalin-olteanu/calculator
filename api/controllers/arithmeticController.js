db610619a9c6a2c2713010d9b26bfd0624da2cd3
catalin-olteanu <47027177+catalin-olteanu@users.noreply.github.com>
2020-05-05 03:13:25 -0700

    Update arithmeticController.js

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    //needed for string concat avoidances
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
